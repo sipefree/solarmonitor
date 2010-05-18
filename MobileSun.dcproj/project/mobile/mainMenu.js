48c9220dd0d19a5b0480b7a955e3e47a868a1c8b
Simon Free <sipefree@gmail.com>
2010-05-18 15:29:18 +0100

    First commit

---
+++ MobileSun.dcproj/project/mobile/mainMenu.js
@@ -0,0 +1,19 @@
+{
+    title: 'Solar Monitor',
+    mainMenu: [
+        { name: "Current Images", image: "Images/satellite.png" },
+        { name: "Current Video", image: "Images/filmstrip.png" }, 
+        { name: "Current Graphs", image: "Images/graph.png" }, 
+        { name: "Browse Archives", image: "Images/folder.png" },
+        { name: "Forecast", image: "Images/folder.png" },
+         
+    ],
+    imageSources: [
+        { name: "MDI Mag", image: "Images/mdi_mag.png" },
+        { name: "MDI Cont", image: "Images/mdi_cont.png" },
+        { name: "GHN Hα", image: "Images/ghn_ha.png" },
+        { name: "EIT 171Å", image: "Images/eit_171a.png" },
+        { name: "EIT 195Å", image: "Images/eit_195a.png" },
+        { name: "XRT", image: "Images/xrt.png" },
+    ]
+}
